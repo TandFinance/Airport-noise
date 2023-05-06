599c1bac120c12eb09794dc2df6a00b2de14360d
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-06 02:21:05 +0200

    Update aerodrome_noise.py

--- aerodrome_noise.py
+++ aerodrome_noise.py
@@ -1,52 +1,63 @@
 import streamlit as st
 import pandas as pd
 
-# Set page title
-st.set_page_config(page_title="Noise.py")
+# Load aircraft options from file
+aircraft_options = pd.read_csv("aircraft_options.csv")["Type"].tolist()
 
-# Load aircraft options from CSV file
-aircraft_options = pd.read_csv("aircraft_option.csv", usecols=["Type"])
+# Set page title and background image
+st.set_page_config(page_title="IFP NOISE", page_icon=None, layout="wide")
+st.markdown(
+    """
+    <style>
+        .main {
+            background-image: url("background.png");
+            background-size: cover;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-# Define input fields
-with st.form(key="input_form"):
-    # Group for parameters
-    st.write("Parameters:")
-    col1, col2, col3, col4 = st.beta_columns(4)
-    with col1:
-        latitude_degree = st.number_input("Latitude (°)", min_value=-90, max_value=90, value=0)
-    with col2:
-        latitude_minute = st.number_input("Minutes", min_value=0, max_value=59, value=0)
-    with col3:
-        latitude_seconde = st.number_input("Seconds", min_value=0, max_value=59, value=0)
-    with col4:
-        latitude_orientation = st.selectbox("Orientation", ["N", "S"])
+# Create parameter groups
+st.markdown("<h1 style='color:blue; font-weight:bold;'>IFP NOISE</h1>", unsafe_allow_html=True)
+with st.beta_container():
+    st.markdown("<h2 style='color:white; font-weight:bold;'>Parameters</h2>", unsafe_allow_html=True)
 
-    col1, col2 = st.beta_columns(2)
-    with col1:
-        rayon = st.slider("Rayon (m)", min_value=100, max_value=10000, step=20, value=500)
-    with col2:
-        pas = st.number_input("Pas (m)", min_value=50, value=100)
+    with st.beta_container():
+        st.markdown("<h3 style='color:white;'>Coordonnées de points de réference</h3>", unsafe_allow_html=True)
+        lat_deg = st.number_input("Latitude (degré)", value=0, step=1)
+        lat_min = st.number_input("Latitude (minute)", value=0, step=1)
+        lat_sec = st.number_input("Latitude (seconde)", value=0, step=1)
+        lat_dir = st.selectbox("Latitude (direction)", ("Nord", "Sud"))
 
-    temperature = st.number_input("Temperature (°C)", value=20.0)
+        lon_deg = st.number_input("Longitude (degré)", value=0, step=1)
+        lon_min = st.number_input("Longitude (minute)", value=0, step=1)
+        lon_sec = st.number_input("Longitude (seconde)", value=0, step=1)
+        lon_dir = st.selectbox("Longitude (direction)", ("Est", "Ouest"))
 
-    densite = st.number_input("Densité", min_value=0.7, max_value=2.0, value=1.0)
+    with st.beta_container():
+        st.markdown("<h3 style='color:white;'>Rayon (m)</h3>", unsafe_allow_html=True)
+        rayon = st.slider("Rayon", min_value=100, max_value=2000, step=20, value=100)
 
-    # Group for aircraft options
-    st.write("Aircraft:")
-    aircraft_choice = st.selectbox("Mouvements", aircraft_options["Type"])
+    with st.beta_container():
+        st.markdown("<h3 style='color:white;'>Pas (m)</h3>", unsafe_allow_html=True)
+        pas = st.slider("Pas", min_value=50, max_value=1000, step=10, value=100)
 
-    # Submit button
-    submitted = st.form_submit_button(label="Submit")
+    with st.beta_container():
+        st.markdown("<h3 style='color:white;'>T°C</h3>", unsafe_allow_html=True)
+        temperature = st.number_input("T°C", value=0)
 
-# Show results if submitted
-if submitted:
-    st.write("Parameters:")
-    st.write(f"Latitude: {latitude_degree}° {latitude_minute}' {latitude_seconde}\" {latitude_orientation}")
-    st.write(f"Rayon: {rayon}m")
-    st.write(f"Pas: {pas}m")
-    st.write(f"Temperature: {temperature}°C")
-    st.write(f"Densité: {densite}")
+    with st.beta_container():
+        st.markdown("<h3 style='color:white;'>Densité</h3>", unsafe_allow_html=True)
+        densite = st.slider("Densité", min_value=0.7, max_value=2.0, step=0.1, value=1.0)
 
-    st.write("Aircraft:")
-    st.write(f"Type: {aircraft_choice}")
+    with st.beta_container():
+        st.markdown("<h3 style='color:white;'>Aircraft</h3>", unsafe_allow_html=True)
+        aircraft_type = st.selectbox("Type", options=aircraft_options)
+        mouvement = st.selectbox("Mouvements", options=["Arrivée", "Départ"])
 
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    with st.beta_container():
+        st.button("Calculate Noise")
