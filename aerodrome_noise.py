c6e411f91bac929874d256a457c65577eba8de75
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-06 02:05:29 +0200

    Update aerodrome_noise.py

--- aerodrome_noise.py
+++ aerodrome_noise.py
@@ -1,44 +1,59 @@
 import streamlit as st
 import pandas as pd
-# Set page config
-st.set_page_config(page_title="IFP NOISE", page_icon=":sound:", layout="wide")
 
-# Set background image
-main_bg = "background.png"
-st.markdown(f"""
-    <style>
-        .reportview-container {{
-            background: url({main_bg}) no-repeat center center fixed;
-            background-size: cover;
-        }}
-    </style>
-""", unsafe_allow_html=True)
+# Load aircraft options from CSV
+aircraft_options = pd.read_csv('aircraft_option.csv')['Type'].tolist()
 
-# Add title
-st.markdown("<h1 style='text-align: center; color: blue; font-weight: bold;'>IFP NOISE</h1>", unsafe_allow_html=True)
+# Set up page layout and background image
+st.set_page_config(page_title="Noise", page_icon=":sound:", layout="wide")
+bg_image = "background.png"
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_960_720.jpg");
+background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Add groups
-parameters = st.beta_expander("Parameters")
-mouvements = st.beta_expander("Mouvements")
+# Add page title
+st.markdown("<h1 style='text-align: center; color: blue;'>IFP NOISE</h1>", unsafe_allow_html=True)
 
-# Add inputs to parameters group
-with parameters:
-    st.subheader("Reference Coordinate")
-    col1, col2 = st.beta_columns(2)
+# Add field parameters group
+with st.beta_container():
+    st.header("Field Parameters")
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
     with col1:
-        lat = st.number_input("Latitude (deg)", value=0, step=1)
+        lat_deg = st.number_input("Latitude (deg)", min_value=0, max_value=90)
     with col2:
-        lon = st.number_input("Longitude (deg)", value=0, step=1)
+        lat_min = st.number_input("Latitude (min)", min_value=0, max_value=59)
+    with col3:
+        lat_sec = st.number_input("Latitude (sec)", min_value=0, max_value=59)
+    with col4:
+        rayon = st.number_input("Rayon (km)", min_value=0, max_value=999)
+    with col5:
+        pas = st.number_input("Pas (m)", min_value=0, max_value=999)
 
-    st.number_input("Rayon (km)", value=0, step=1)
-    st.number_input("Pas (m)", value=0, step=1)
-    st.number_input("T°C", value=0, step=1)
-    st.number_input("Densité", value=0, step=1)
+    st.number_input("T°C", min_value=0, max_value=999)
+    st.number_input("Densité", min_value=0, max_value=999)
 
-# Add combobox to mouvements group
-with mouvements:
-    st.subheader("Aircraft")
-    # Read the CSV file using Pandas
-    aircraft_options = pd.read_csv("aircraft_option.csv")["Type"].tolist()
-    aircraft_choice = st.selectbox("Mouvements", ["Arrivée", "Départ"])
-    aircraft_type = st.selectbox("Type", aircraft_options)
+# Add aircraft group with movement combobox
+with st.beta_container():
+    st.header("Aircraft")
+    mouvements = st.selectbox("Mouvements", ["Arrivée", "Départ"])
+    aircraft_type = st.selectbox("Aircraft Type", aircraft_options)
+
+# Add a button to submit the form
+if st.button("Submit"):
+    # Combine latitude inputs into single value
+    latitude = lat_deg + (lat_min / 60) + (lat_sec / 3600)
+
+    # Print form values to console
+    print("Latitude:", latitude)
+    print("Rayon:", rayon)
+    print("Pas:", pas)
+    print("T°C:", st.session_state["T°C"])
+    print("Densité:", st.session_state["Densité"])
+    print("Mouvements:", mouvements)
+    print("Aircraft Type:", aircraft_type)
