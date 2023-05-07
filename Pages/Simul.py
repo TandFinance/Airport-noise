d579e38098599ae37565b49c273cad937cdd6160
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-07 23:05:23 +0100

    Update Simul.py

--- Pages/Simul.py
+++ Pages/Simul.py
@@ -1,4 +1,12 @@
-import streamlit
+import streamlit as st
 import sys
 sys.path.append("..")
-from aerodrome_noise import typ
+from aerodrome_noise import aircraft_type
+st.set_page_config(page_title="IFP NOISE", page_icon=":sound:", layout="wide", initial_sidebar_state="expanded")
+Title="IFP NOISE"
+Profil="background.png"
+with st.container ():
+    st.title(Title)
+    st.image(Profil)
+with st.container():
+  st.markdown(aircraft_type)
