025d5b4e9b6e582f719584565cca1082f25b7c93
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-07 12:43:36 +0200

    Update aerodrome_noise.py

--- aerodrome_noise.py
+++ aerodrome_noise.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import pandas as pd
 
+st.markdown(
+    """
+    <style>
+    .verticalLine {
+        border-left: 2px solid blue;
+        height: 100%;
+        position:absolute;
+        left: 50%;
+        margin-left: -3px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 # Read aircraft options from file
 aircraft_options = pd.read_csv("aircraft_option.csv")["Type"].tolist()
 
@@ -27,7 +42,7 @@ with st.beta_container():
     with st.beta_container():
         st.markdown("## Coordonnées de points de réference")
         st.markdown("<hr style='border-top: 2px solid blue;'>", unsafe_allow_html=True)
-        col1, col2 = st.beta_columns(2)
+        col1, colb, col2 = st.beta_columns([3, 0.5, 3])
         with col1 :
             st.markdown("## Latitude")
             with st.beta_container():
@@ -40,6 +55,8 @@ with st.beta_container():
                     lat_sec = st.number_input("Latitude (secondes)",min_value=0, max_value=90, value=0, step=1, key="lat_sec")
                 with col[3] :
                     lat_dir = st.selectbox("Latitude (direction)", ["Nord", "Sud"], key="lat_dir")
+         with colb :
+            st.markdown('<div class="verticalLine"></div>', unsafe_allow_html=True)
         with col2 :
             st.markdown("## Longitude")
             with st.beta_container():
@@ -52,13 +69,15 @@ with st.beta_container():
                     lon_sec = st.number_input("Longitude (secondes)",min_value=0, max_value=90, value=0, step=1, key="lon_sec")
                 with colg[3]:
                     lon_dir = st.selectbox("Longitude (direction)", ["Est", "Ouest"], key="lon_dir")
+    st.markdown("<hr style='border-top: 2px solid blue;'>", unsafe_allow_html=True)
     with st.beta_container():
         st.markdown("## Autres paramètres")
         rayon = st.slider("Rayon (m)", min_value=100, max_value=1000, step=20, value=100)
         pas = st.slider("Pas (m)", min_value=50, max_value=500, step=10, value=100)
         temp = st.number_input("Température (°C)", value=0.0, step=0.1, format="%.1f")
         densite = st.number_input("Densité", min_value=0.7, max_value=2.0, step=0.1, value=1.0, format="%.1f")
-
+   
+st.markdown("<hr style='border-top: 2px solid blue;'>", unsafe_allow_html=True)
     with st.beta_container():
         st.markdown("## Type d'avion")
         aircraft_type = st.selectbox("Type", aircraft_options)
