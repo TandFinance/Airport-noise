96864f85e4fda07bdce61f8592804edbc5705ebc
Moustapha Amadou Roufaï <moustapharouf@yahoo.fr>
2023-05-09 19:54:11 +0100

    Update aerodrome_noise.py

--- aerodrome_noise.py
+++ aerodrome_noise.py
@@ -9,7 +9,6 @@ def dms_to_dd(d: float, m: float, s: float, dir: str) -> float:
         dd = -dd
     return dd
 #**
-@st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
